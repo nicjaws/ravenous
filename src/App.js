6352149d386f9d6583c131a0c7bc8a89f12806b3
Nicolas Corbalan <outreef@hotmail.com>
2017-12-04 20:13:15 +0100

    Fixing errors in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 import './App.css';
@@ -26,16 +26,17 @@ const businesses = [
 ]; 
 
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
   <h1>ravenous</h1>
     <SearchBar/>
-    <BusinessList propBusinesses={businesses}/>
+    <BusinessList Businesses={businesses}/>
 </div>
     );
   }
 }
 
 export default App;
+
